1318ad3b2cf4768e8d885025e92094911023b497
fatchan <tom@69420.me>
2019-03-01 13:33:25 +1100

    trim star and border URLs and handle missing stars URL edge case

--- src/basicinfo.js
+++ src/basicinfo.js
@@ -2,17 +2,19 @@
 
 const cheerio = require('cheerio')
 	, getHtml = require('./gethtml.js');
-	
+
 module.exports = async (battletag, platform, html) => {
-	
+
 	const $ = cheerio.load(html || await getHtml(battletag, platform));
 
-	//this URL is for no stars
-	let starsURL = $('.player-rank').attr('style').substring(21, $('.player-rank').attr('style').length-1);
-	if (starsURL == 'https://d1u1mce87gyfbn.cloudfront.net/game/playerlevelrewards/0x025000000000095F_Rank.png') {
-		starsURL = ''; 
+	let starsURL = $('.player-rank').attr('style')
+	if (starsURL) {
+		starsURL = starsURL.substring(21, $('.player-rank').attr('style').length-1).trim();
+		if (starsURL === 'https://d1u1mce87gyfbn.cloudfront.net/game/playerlevelrewards/0x025000000000095F_Rank.png') {
+			starsURL = '';
+		}
 	}
-	
+
 	const data = {
 		battletag: battletag,
 		rank: $('.competitive-rank').find('div').first().text(),
@@ -26,7 +28,7 @@ module.exports = async (battletag, platform, html) => {
 		profileURL: encodeURI(`https://playoverwatch.com/en-us/career/${platform}/${battletag}`),
 		iconURL: $('.player-portrait').attr('src'),
 		rankIconURL: $('.competitive-rank').find('img').attr('src'),
-		borderURL: $('.player-level').attr('style').substring(21, $('.player-level').attr('style').length-1),
+		borderURL: $('.player-level').attr('style').substring(21, $('.player-level').attr('style').length-1).trim(),
 		starsURL: starsURL
 	}
 
