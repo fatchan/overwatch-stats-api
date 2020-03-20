e7bdf11c001a7b29ee91ab0a9908b4f4a89ddefa
Federico Grandi <fgrandi30@gmail.com>
2020-03-20 20:13:08 +0000

    [auto] Lint source code

--- src/basicinfo.js
+++ src/basicinfo.js
@@ -1,54 +1,54 @@
 'use strict';
 
 const cheerio = require('cheerio')
-	, getHtml = require('./gethtml.js')
-	, getPrestige = require('./prestige.js');
+  , getHtml = require('./gethtml.js')
+  , getPrestige = require('./prestige.js');
 
 module.exports = async (battletag, platform, html) => {
 
-	const $ = cheerio.load(html || await getHtml(battletag, platform));
+  const $ = cheerio.load(html || await getHtml(battletag, platform));
 
-	let borderURL = $('.player-level').attr('style').substring(21, $('.player-level').attr('style').length - 1).trim();
-	let starsURL = $('.player-rank').attr('style')
-	if (starsURL) {
-		starsURL = starsURL.substring(21, $('.player-rank').attr('style').length - 1).trim();
-		if (starsURL === 'https://d1u1mce87gyfbn.cloudfront.net/game/playerlevelrewards/0x025000000000095F_Rank.png') {
-			starsURL = '';
-		}
-	}
+  const borderURL = $('.player-level').attr('style').substring(21, $('.player-level').attr('style').length - 1).trim();
+  let starsURL = $('.player-rank').attr('style');
+  if (starsURL) {
+    starsURL = starsURL.substring(21, $('.player-rank').attr('style').length - 1).trim();
+    if (starsURL === 'https://d1u1mce87gyfbn.cloudfront.net/game/playerlevelrewards/0x025000000000095F_Rank.png') {
+      starsURL = '';
+    }
+  }
 
-	let rank = {};
+  const rank = {};
 
-	$('.competitive-rank-role').each((index, element) => {
-		let roleIcon = $(element).find('.competitive-rank-role-icon').first().attr('src');
-		let roleName = $(element).find('.competitive-rank-tier-tooltip').attr('data-ow-tooltip-text').split(' ')[0].toLowerCase();
-		let tierIcon = $(element).find('.competitive-rank-tier-icon').attr('src');
-		let sr = $(element).find('.competitive-rank-level').text();
+  $('.competitive-rank-role').each((index, element) => {
+    const roleIcon = $(element).find('.competitive-rank-role-icon').first().attr('src');
+    const roleName = $(element).find('.competitive-rank-tier-tooltip').attr('data-ow-tooltip-text').split(' ')[0].toLowerCase();
+    const tierIcon = $(element).find('.competitive-rank-tier-icon').attr('src');
+    const sr = $(element).find('.competitive-rank-level').text();
 
-		rank[roleName] = {
-			sr,
-			roleIcon,
-			tierIcon
-		}
-	});
+    rank[roleName] = {
+      sr,
+      roleIcon,
+      tierIcon
+    };
+  });
 
-	const data = {
-		battletag,
-		rank,
-		level: $('.player-level').find('div').first().text(),
-		prestige: getPrestige(borderURL, starsURL) || '',
-		endorsementLevel: ($('.endorsement-level').first().next().text() || 0),
-		endorsements: {
-			shotcaller: (($('.EndorsementIcon-border--shotcaller').attr('data-value') * 100).toFixed() || 0),
-			teammate: (($('.EndorsementIcon-border--teammate').attr('data-value') * 100).toFixed() || 0),
-			sportsmanship: (($('.EndorsementIcon-border--sportsmanship').attr('data-value') * 100).toFixed() || 0)
-		},
-		profileURL: encodeURI(`https://playoverwatch.com/en-us/career/${platform}/${battletag}`),
-		iconURL: $('.player-portrait').attr('src'),
-		borderURL,
-		starsURL
-	}
+  const data = {
+    battletag,
+    rank,
+    level: $('.player-level').find('div').first().text(),
+    prestige: getPrestige(borderURL, starsURL) || '',
+    endorsementLevel: ($('.endorsement-level').first().next().text() || 0),
+    endorsements: {
+      shotcaller: (($('.EndorsementIcon-border--shotcaller').attr('data-value') * 100).toFixed() || 0),
+      teammate: (($('.EndorsementIcon-border--teammate').attr('data-value') * 100).toFixed() || 0),
+      sportsmanship: (($('.EndorsementIcon-border--sportsmanship').attr('data-value') * 100).toFixed() || 0)
+    },
+    profileURL: encodeURI(`https://playoverwatch.com/en-us/career/${platform}/${battletag}`),
+    iconURL: $('.player-portrait').attr('src'),
+    borderURL,
+    starsURL
+  };
 
-	return data;
+  return data;
 
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const getHtml = require('./gethtml.js')
-	, getBasicInfo = require('./basicinfo.js')
-	, getHeroStats = require('./herostats.js')
-	, getMostPlayed = require('./mostplayed.js');
+  , getBasicInfo = require('./basicinfo.js')
+  , getHeroStats = require('./herostats.js')
+  , getMostPlayed = require('./mostplayed.js');
 
 async function getAllStats(battletag, platform) {
 	
-	const html = await getHtml(battletag, platform);
+  const html = await getHtml(battletag, platform);
 	
-	const basicInfo = await module.exports.getBasicInfo(battletag, platform, html);
-	const heroStats = await module.exports.getHeroStats(battletag, platform, html);
-	const mostPlayed = await module.exports.getMostPlayed(battletag, platform, html);
+  const basicInfo = await module.exports.getBasicInfo(battletag, platform, html);
+  const heroStats = await module.exports.getHeroStats(battletag, platform, html);
+  const mostPlayed = await module.exports.getMostPlayed(battletag, platform, html);
 	
-	return {...basicInfo, heroStats: heroStats, mostPlayed: mostPlayed}
+  return {...basicInfo, heroStats: heroStats, mostPlayed: mostPlayed};
 	
 }
 
 module.exports = {
-	getAllStats: getAllStats, 
-	getBasicInfo: getBasicInfo, 
-	getHeroStats: getHeroStats, 
-	getMostPlayed: getMostPlayed
-}
+  getAllStats: getAllStats, 
+  getBasicInfo: getBasicInfo, 
+  getHeroStats: getHeroStats, 
+  getMostPlayed: getMostPlayed
+};
